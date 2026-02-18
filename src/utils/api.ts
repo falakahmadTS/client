439d958300d33b81a3677d39c75e61ddecf3a2b0
falakahmadTS <falak.ahmad@tycoonzsolutions.com>
2026-02-18 17:53:38 +0500

    updated links

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5005',
-    // baseURL: 'https://server-cyan-chi-30.vercel.app',
+    // baseURL: 'http://localhost:5005',
+    baseURL: 'https://server-cyan-chi-30.vercel.app',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
